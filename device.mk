a0644892b925ef2c1ddf7907d431cf26e8c82cda
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-08-16 23:13:58 +0300

    Mi439: Build MindTheGapps
    * https://github.com/luk1337/android_vendor_extra/blob/73ef39f9d53e570448f62fe9414ea944a77740c3/product.mk#L19
    
    * https://gitlab.com/MindTheGapps/vendor_gapps
    
    Co-Authored-By: luk1337 <priv.luk@gmail.com>

--- device.mk
+++ device.mk
@@ -125,8 +125,13 @@ $(call inherit-product, packages/apps/GalleryGo/config.mk)
 endif
 
 # GMS
-ifeq ($(WITH_GMS),true)
-GMS_MAKEFILE=gms_minimal.mk
+# ifeq ($(WITH_GMS),true)
+# GMS_MAKEFILE=gms_minimal.mk
+# endif
+
+# Google Apps
+ifeq ($(WITH_GMS), true)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 
 # Inherit from vendor blobs
