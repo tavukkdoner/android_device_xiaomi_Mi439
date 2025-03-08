2fe61b9d908416d41e45e1051555c242f5054a14
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-03-08 23:18:10 +0200

    Mi439: add camera shim for _ZN14SeeMoreWrapper*
    * Error opening frameproc library libmmcamera_llvd.so error dlopen failed: cannot locate symbol "_ZN14SeeMoreWrapper20SeeMoreWrapperStatusC1Ev" referenced by "/vendor/lib/libllvd_smore.so"...
    
    * Error opening frameproc library libmmcamera_sw_tnr.so error dlopen failed: cannot locate symbol "_ZN14SeeMoreWrapper11_djNsepNBJYEPKhS1_jjS1_S1_jjPhS2_jj" referenced by "/vendor/lib/libllvd_sw_tnr.so"...

--- libshims/camera_Mi439_shim.cpp
+++ libshims/camera_Mi439_shim.cpp
@@ -2,6 +2,8 @@
 
 namespace android {
 	
+	extern "C" void _ZN14SeeMoreWrapper11_djNsepNBJYEPKhS1_jjS1_S1_jjPhS2_jj() { };
 	extern "C" void _ZN14SeeMoreWrapper11_wM8GQx3FTjEbbfffjjffffffbfffffbbffbbff() { };
 	extern "C" void _ZN14SeeMoreWrapper11_yu3oaTHCJgEv() { };
+	extern "C" void _ZN14SeeMoreWrapper20SeeMoreWrapperStatusC1Ev() { };
 }
