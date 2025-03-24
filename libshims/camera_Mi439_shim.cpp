5cea5384a83ca14a6b2065aeb2a6d63ac0d61259
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-03-24 22:28:04 +0200

    Mi439: add camera shim for _ZN14SeeMoreWrapper*

--- libshims/camera_Mi439_shim.cpp
+++ libshims/camera_Mi439_shim.cpp
@@ -3,7 +3,12 @@
 namespace android {
 	
 	extern "C" void _ZN14SeeMoreWrapper11_djNsepNBJYEPKhS1_jjS1_S1_jjPhS2_jj() { };
+	extern "C" void _ZN14SeeMoreWrapper11_mccqQX1XplERfS0_S0_S0_() { };
 	extern "C" void _ZN14SeeMoreWrapper11_wM8GQx3FTjEbbfffjjffffffbfffffbbffbbff() { };
 	extern "C" void _ZN14SeeMoreWrapper11_yu3oaTHCJgEv() { };
+	extern "C" void _ZN14SeeMoreWrapper20SeeMoreWrapperStatusaSERKS0_() { };
+	extern "C" void _ZN14SeeMoreWrapper20SeeMoreWrapperStatusD1Ev() { };
 	extern "C" void _ZN14SeeMoreWrapper20SeeMoreWrapperStatusC1Ev() { };
+	extern "C" void _ZNK14SeeMoreWrapper20SeeMoreWrapperStatus12getErrorTypeEv() { };
+	extern "C" void _ZNK14SeeMoreWrapper20SeeMoreWrapperStatus9isSuccessEv() { };
 }
