8b053031998b289b1af59cf6ae8406bb055ee7bb
Bruno Martins <bgcngm@gmail.com>
2025-05-04 15:01:00 +0300

    Mi439: Add MindTheGapps for EROFS

--- device.mk
+++ device.mk
@@ -138,6 +138,11 @@ ifeq ($(WITH_GOOGLE_CAM_GO),true)
 $(call inherit-product, packages/apps/GalleryGo/config.mk)
 endif
 
+# Google Apps
+ifeq ($(TARGET_USES_EROFS), true)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+endif
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/Mi439/Mi439-vendor.mk)
 
