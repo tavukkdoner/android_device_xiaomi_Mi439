2dd8e84dfb89771b1bfdda06a38bd26ca14fbd16
Bruno Martins <bgcngm@gmail.com>
2025-05-10 19:30:49 +0300

    Mi439: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,6 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest/gatekeeper.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_mi439
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_mi439
 
 # Kernel
