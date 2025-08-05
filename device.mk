520cde2acd061160a3fbb1ecd20447348865a31d
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-08-05 22:46:04 +0300

    Mi439: bringup AxionAOSP

--- device.mk
+++ device.mk
@@ -120,8 +120,6 @@ PRODUCT_SOONG_NAMESPACES += \
     packages/apps/RevampedFMRadio/jni \
     $(LOCAL_PATH)
 
-# Viper
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Google Camera Go
 $(call inherit-product, packages/apps/CameraGo/config.mk)
--- lineage_Mi439_4_19.mk
+++ lineage_Mi439_4_19.mk
@@ -12,6 +12,46 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# EPPE
+TARGET_DISABLE_EPPE := true
+
+# Rear camera specs
+AXION_CAMERA_REAR_INFO := 5
+
+# Front camera specs
+AXION_CAMERA_FRONT_INFO := 13
+
+# Maintainer name
+AXION_MAINTAINER := tavukkdoner
+
+# Processor name
+AXION_PROCESSOR := SDM439
+
+# Define small and big core groups
+AXION_CPU_SMALL_CORES := 4,5,6,7
+AXION_CPU_BIG_CORES := 0,1,2,3
+# Used by cpu limiter and performance mode
+# Background cores used for non-critical cpusets 
+AXION_CPU_BG := 5-7
+# Background cores used for foreground cpusets
+AXION_CPU_FG := 0-7
+# CPU cores that will be used when limiting other cpusets except top-app
+AXION_CPU_LIMIT_BG := 6-7
+# CPUset that will be used to unlimit critical cpusets for UI
+AXION_CPU_UNLIMIT_UI = 0-7
+# CPUset that will be used when limiting critical cpusets for UI
+AXION_CPU_LIMIT_UI = 4-7
+# CPUset that will be used for critical display processes
+AXION_CPU_DISPLAY = 0-4
+# CPUset that will be used for audio processes e.g. audioserver
+AXION_CPU_AUDIO = 4-7
+# ViperFX
+TARGET_INCLUDE_VIPERFX := true
+# Matlog
+TARGET_INCLUDE_MATLOG := true
+# LOS Prebuilts
+TARGET_INCLUDES_LOS_PREBUILTS = true
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
