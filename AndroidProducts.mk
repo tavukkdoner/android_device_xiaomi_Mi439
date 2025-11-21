52b5c5a1f7707f560ddfaa6470c060759a16c5a3
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-11-21 21:15:09 +0200

    Mi439: Bringup MicaOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+    $(LOCAL_DIR)/mica_Mi439.mk \
+    $(LOCAL_DIR)/mica_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    mica_Mi439-user \
+    mica_Mi439-userdebug \
+    mica_Mi439-eng \
+    mica_Mi439_4_19-user \
+    mica_Mi439_4_19-userdebug \
+    mica_Mi439_4_19-eng
