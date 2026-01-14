de948c29e1172cbb17c138c37d221065651d8e8f
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-01-14 04:24:21 +0200

    Mi439: Fix camera unused variable errors
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3HWI.cpp:3572:22: error: variable 'camHandle' set but not used [-Werror,-Wunused-but-set-variable]
     3572 |             uint32_t camHandle = mCameraHandle->camera_handle;
          |                      ^
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3HWI.cpp:3573:22: error: variable 'chHandle' set but not used [-Werror,-Wunused-but-set-variable]
     3573 |             uint32_t chHandle = mChannelHandle;
          |                      ^
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3HWI.cpp:6954:14: error: variable 'channelHdl' set but not used [-Werror,-Wunused-but-set-variable]
     6954 |     uint32_t channelHdl = mChannelHandle;
          |              ^
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3HWI.cpp:11146:21: error: variable 'written_len' set but not used [-Werror,-Wunused-but-set-variable]
     11146 |             ssize_t written_len = 0;
    
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3Channel.cpp:4935:14: error: variable 'frameIndex' set but not used [-Werror,-Wunused-but-set-variable]
     4935 |     uint32_t frameIndex;
          |              ^
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3Channel.cpp:7076:14: error: variable 'frameIndex' set but not used [-Werror,-Wunused-but-set-variable]
     7076 |     uint32_t frameIndex;
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3PostProc.cpp:4007:32: error: variable 'hal_obj' set but not used [-Werror,-Wunused-but-set-variable]
     4007 |     QCamera3HardwareInterface* hal_obj = NULL;
          |                                ^
    device/xiaomi/Mi439/camera/QCamera2/QCamera2Factory.cpp:613:10: error: variable 'rc' set but not used [-Werror,-Wunused-but-set-variable]
      613 |     bool rc = false;
          |          ^
    device/xiaomi/Mi439/camera/QCamera2/HAL3/QCamera3Mem.cpp:1247:10: error: variable 'needToInvalidate' set but not used [-Werror,-Wunused-but-set-variable]
     1247 |     bool needToInvalidate = false;
          |          ^
    device/xiaomi/Mi439/camera/QCamera2/util/QCameraHALPP.cpp:403:17: error: variable 'writen_bytes' set but not used [-Werror,-Wunused-but-set-variable]
      403 |         ssize_t writen_bytes = 0;
          |                 ^
    device/xiaomi/Mi439/camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c:3265:21: error: variable 'temp_rc' set but not used [-Werror,-Wunused-but-set-variable]
     3265 |             int32_t temp_rc = 0;
          |                     ^
    device/xiaomi/Mi439/camera/QCamera2/util/QCameraBokeh.cpp:827:13: error: variable 'written_len' set but not used [-Werror,-Wunused-but-set-variable]
      827 |     ssize_t written_len = 0;
          |             ^
    device/xiaomi/Mi439/camera/QCamera2/stack/mm-camera-interface/src/mm_camera_muxer.c:1749:13: error: variable 'rc' set but not used [-Werror,-Wunused-but-set-variable]
     1749 |     int32_t rc = 0;
          |             ^

--- camera/QCamera2/util/QCameraHALPP.cpp
+++ camera/QCamera2/util/QCameraHALPP.cpp
@@ -403,7 +403,7 @@ void QCameraHALPP::dumpYUVtoFile(const uint8_t* pBuf, const char *name, ssize_t 
         ssize_t writen_bytes = 0;
         writen_bytes = write(file_fd, pBuf, buf_len);
         close(file_fd);
-        LOGD("dump output frame to file: %s, size:%d", name, buf_len);
+        LOGD("dump output frame to file: %s, size:%d, writen_bytes:%d", name, buf_len, writen_bytes);
     }
 
     LOGD("X.");
