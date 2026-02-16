8b5c45c63a9544bc8a15c91a869a9c5925fa0817
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-02-16 07:52:59 +0200

    Revert "Mi439: Drop reserved partition"
    This reverts commit 43b379a2477a1765660a9d3db553dfcee496463c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,10 @@ $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 41943040)) # 40 MB
 
+ifneq ($(WITH_GMS),true)
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1048576000 # 1000 MB
+endif
+
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/dt2w"
 
